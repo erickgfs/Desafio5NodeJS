7e721edd35e18d8917b8abcaed9766c599b52d91
Erick Giovanni <erickgfs@gmail.com>
2020-08-27 13:17:22 -0300

    Desafio 5 finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+import { uuid } from 'uuidv4';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface TransactionDTO {
+  title: string;
+  type: 'outcome' | 'income';
+  value: number;
 }
 
 class TransactionsRepository {
@@ -14,15 +21,49 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let income = 0;
+    let outcome = 0;
+
+    this.transactions.forEach(res => {
+      if (res.type === 'income') {
+        income += res.value;
+      } else {
+        outcome += res.value;
+      }
+    });
+
+    const total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const { total } = this.getBalance();
+
+    if (total < value && type === 'outcome') {
+      throw Error('insufficient funds');
+    }
+
+    const transaction = {
+      id: uuid(),
+      title,
+      value,
+      type,
+    };
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
